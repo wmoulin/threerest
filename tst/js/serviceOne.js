5c9609d0a11a0a40500b989c3ab67aad60daee20
wmoulin <moulin.wilfried@gmail.com>
2016-02-03 14:23:43 +0100

    proto

--- tst/js/serviceOne.js
+++ tst/js/serviceOne.js
@@ -1,12 +1,11 @@
 import Service from "../../src/js/service";
 import Method from "../../src/js/methods/method";
 
-@Service.path("test")
+@Service.path("/test")
 export default class ServiceTest {
 
   @Method.get("/");
   testGet(req, res, next) {
-    console.log("coucou");
     res.send("coucou");
   }
 
