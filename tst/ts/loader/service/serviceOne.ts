304c1c8133707347fd47df52becdf9d82ce42077
moulin wilfried <moulin.wilfried@gmail.com>
2019-02-04 23:42:11 +0100

    ajout test not-found-error

--- tst/ts/loader/service/serviceOne.ts
+++ tst/ts/loader/service/serviceOne.ts
@@ -1,5 +1,6 @@
 import Service from "../../../../src/ts/service";
 import { Methods } from "../../../../src/ts/services/methods";
+import NotFoundError from "../../../../src/ts/exceptions/not-found-error";
 import convert from "../../../../src/ts/convert";
 import Param from "../../param";
 
@@ -9,6 +10,9 @@ export default class ServiceTest {
   @Methods.get("/:id")
   @convert(Param)
   testGet(value) {
+    if(value.id == 13) {
+      throw new NotFoundError("13: bad luck")
+    }
     return value;
   }
 }
